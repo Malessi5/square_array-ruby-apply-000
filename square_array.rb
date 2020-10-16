78910b7580ff612328be809eec1c3d9f8c2a1176
Mike Alessi <mikealessi5@gmail.com>
2020-10-16 18:53:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,12 @@
 def square_array(array)
   # your code here
   numbers = [1,2,3]
-  new_numbers = []
+  square_array = []
   counter = 0
 
   numbers.each do |square|
 
-     new_numbers[counter] = square * square
+     square_array[counter] = square * square
      counter += 1
 
   end
