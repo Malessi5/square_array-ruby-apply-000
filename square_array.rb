35f63a001ade4f84d4580501b38fd5a8b9b51767
Mike Alessi <mikealessi5@gmail.com>
2020-10-16 19:10:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   # your code here
   square_array = []
-  array.each do |square|
-    square_array.push square*square
+
+  square_array.each do |square|
+    square_array.push square**2
 
   end
 end
